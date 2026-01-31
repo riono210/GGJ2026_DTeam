e57c0099070722bd6f87df0bcb3f5767ac142ca8
riono210 <e165729@ie.u-ryukyu.ac.jp>
2026-01-31 20:02:47 +0900

    Add ObstacleType

--- Assets/Member/Yasuda/Scripts/ObstacleObject.cs
+++ Assets/Member/Yasuda/Scripts/ObstacleObject.cs
@@ -1,7 +1,19 @@
+using System;
 using UnityEngine;
+
+public enum ObstacleType
+{
+    None = 0,
+    TypeA = 1,
+    TypeB = 2,
+    TypeC = 3,
+}
 
 public class ObstacleObject : IMoveObject
 {
+    [SerializeField] private ObstacleType obstacleType;
+    ObstacleType ObstacleType => obstacleType;
+    
     // // Start is called once before the first execution of Update after the MonoBehaviour is created
     // void Start()
     // {
