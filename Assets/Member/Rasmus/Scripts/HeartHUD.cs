d894fe08603a2d24cd6d5fc6a47e40a67c4e7cd8
riono210 <e165729@ie.u-ryukyu.ac.jp>
2026-02-01 17:42:27 +0900

    heart fix

--- Assets/Member/Rasmus/Scripts/HeartHUD.cs
+++ Assets/Member/Rasmus/Scripts/HeartHUD.cs
@@ -7,24 +7,36 @@ public class HeartHUD : MonoBehaviour
 {
     [SerializeField] private List<GameObject> hearts = new List<GameObject>();
     [SerializeField] private GameObject heartPrefab;
-
+    
+    private CompositeDisposable disposables = new CompositeDisposable();
     
     public void RemoveHeart()
     {
         if (hearts.Count > 0)
         {
-            hearts.RemoveAt(hearts.Count);
+            var targetIndex = hearts.Count - 1;
+            var heratObj  = hearts[targetIndex];
+            hearts.RemoveAt(targetIndex);
+            Destroy(heratObj);
         }
     }
 
     private void Start()
     {
+        disposables = new CompositeDisposable();
         PlayerHealth playerHealth = FindAnyObjectByType<PlayerHealth>();
         for (int i = 0; i < playerHealth.currentHeartCount; ++i)
         {
             GameObject heart = Instantiate(heartPrefab, this.transform);
+            hearts.Add(heart);
         }
+        
+        playerHealth.HitObservable.Subscribe(_ => RemoveHeart());
     }
 
-        
+    private void OnDestroy()
+    {
+        disposables.Dispose();
+        disposables = null;
+    }
 }
